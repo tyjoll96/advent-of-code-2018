eb24fe8f0fca391614bcc273cf932b9df2af36ef
Tyler Lloyd <tyjoll96@gmail.com>
2018-12-02 17:33:11 -0800

    First half of the puzzle tested and working

--- tyler/day_1/src/main.rs
+++ tyler/day_1/src/main.rs
@@ -1,3 +1,24 @@
+use std::fs::File;
+use std::io::BufReader;
+use std::io::prelude::*;
+
 fn main() {
-    println!("Hello, world! Test");
+    // Declare variables.
+    let filename = "input.txt".to_string(); // Filename to read data from.
+    let mut frequency: i32 = 0; // "Frequency" to be changed by input.
+
+    // Open the file.
+    let f = File::open(filename).expect("File not found.");
+
+    // Create a BufReader from the file.
+    let f = BufReader::new(f);
+
+    // Iterate over the lines in the file.
+    for line in f.lines() {
+        // Convert the line to a 32-bit integer and add it to the frequency.
+        frequency += line.unwrap().parse::<i32>().unwrap();
+    }
+
+    // Display the end result.
+    println!("The calibrated frequency is: {}", frequency);
 }
