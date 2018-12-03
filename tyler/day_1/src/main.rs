090f5b76d869c8e03e53c0f347f5f11968438fc3
Tyler Lloyd <tyjoll96@gmail.com>
2018-12-02 19:10:32 -0800

    Part two gold star acquired

--- tyler/day_1/src/main.rs
+++ tyler/day_1/src/main.rs
@@ -4,8 +4,14 @@ use std::io::prelude::*;
 
 fn main() {
     // Declare variables.
+    let mut done = false; // Flag for when a frequency has occured twice.
     let filename = "input.txt".to_string(); // Filename to read data from.
     let mut frequency: i32 = 0; // "Frequency" to be changed by input.
+    let mut inputs: Vec<i32> = Vec::new(); // Vector of inputs taken from the input file.
+    let mut prev_frequencies: Vec<i32> = Vec::new(); // Vector to hold previous frequency values.
+
+    // Declare debug variables.
+    let mut iteration = 1;
 
     // Open the file.
     let f = File::open(filename).expect("File not found.");
@@ -15,8 +21,35 @@ fn main() {
 
     // Iterate over the lines in the file.
     for line in f.lines() {
-        // Convert the line to a 32-bit integer and add it to the frequency.
-        frequency += line.unwrap().parse::<i32>().unwrap();
+        // Add them to the inputs vector.
+        inputs.push(line.unwrap().parse::<i32>().unwrap());
+    }
+
+    // Keep iterating over the list of inputs until the second occurance of a frequency is found.
+    while !done {
+        println!("Iteration: {}", iteration);
+        // Iterate over the inputs.
+        for input in inputs.iter() {
+            // Modify the frequency with the current input.
+            frequency += input;
+
+            // Check if a frequency has occured before.
+            if prev_frequencies.contains(&frequency) {
+                // Set the flag to true.
+                done = true;
+
+                // Debug.
+                // println!("A duplicate frequency has occured at: {}", frequency);
+
+                // Stop iterating over the inputs.
+                break;
+            }
+
+            // Add the new frequency to the list of previously seen frequencies.
+            prev_frequencies.push(frequency);
+        }
+
+        iteration += 1;
     }
 
     // Display the end result.
