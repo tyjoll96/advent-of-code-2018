73f39132bd9dab4e6a28bfabdb806457a7decb3d
Tyler Lloyd <tyjoll96@gmail.com>
2018-12-14 17:01:02 -0800

    Creation of vector finalized

--- tyler/src/bin/04_repose_record.rs
+++ tyler/src/bin/04_repose_record.rs
@@ -3,13 +3,15 @@ extern crate regex;
 
 use regex::Regex;
 
+#[derive(PartialEq, Eq, PartialOrd, Ord)]
 struct GuardTimestamp {
-    action: String,
+    year: u16,
+    month: u8,
     day: u8,
     hour: u8,
     minute: u8,
-    month: u8,
-    year: u16
+    id: u16,
+    action: u8
 }
 
 fn parse_input(input: String) -> Vec<GuardTimestamp> {
@@ -17,21 +19,39 @@ fn parse_input(input: String) -> Vec<GuardTimestamp> {
     let mut guard_timestamps: Vec<GuardTimestamp> = Vec::new();
 
     // Create a regex for the input.
-    let re = Regex::new(r"\[(\d{4})\-(\d{2})\-(\d{2}) (\d{2}):(\d{2})\] (.*)").unwrap();
+    let re = Regex::new(r"\[(\d{4})\-(\d{2})\-(\d{2}) (\d{2}):(\d{2})\]( Guard #(\d{1,4}))? (.*)").unwrap();
 
     // Iterate over input that matches the regex.
     for cap in re.captures_iter(&input) {
-        // Add the fabric plan to the collection of fabric plans.
+        let mut guard_id = 0;
+        let mut guard_action = 0;
+
+        // Determine the values of ID and action.
+        if cap[8].contains("falls") {
+            guard_action = 2;
+        } else if cap[8].contains("wakes") {
+            guard_action = 3;
+        } else {
+            guard_id = cap[7].parse::<u16>().unwrap();
+            guard_action = 1;
+        }
+
+        // Add the guard timestamp to the collection of guard timestamps.
         guard_timestamps.push(GuardTimestamp {
-            action: cap[6].to_string(),
+            year: cap[1].parse::<u16>().unwrap(),
+            month: cap[2].parse::<u8>().unwrap(),
             day: cap[3].parse::<u8>().unwrap(),
             hour: cap[4].parse::<u8>().unwrap(),
             minute: cap[5].parse::<u8>().unwrap(),
-            month: cap[2].parse::<u8>().unwrap(),
-            year: cap[1].parse::<u16>().unwrap()
+            id: guard_id,
+            action: guard_action
         });
     }
 
+    // Order guard timestamps.
+    guard_timestamps.sort();
+
+    // Return guard timestamps.
     guard_timestamps
 }
 
